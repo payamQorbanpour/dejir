2f4c45d27ad3ce272e836c343458f4c902c118ab
Payam Qorbanpour <payamqorbanpour@gmail.com>
2020-12-14 22:06:32 +0330

    Add nonspam

--- bot.py
+++ bot.py
@@ -30,9 +30,14 @@ def cancel(bot, update):
 # states
 msg = range(1)
 
-def callback(bot, update):
-    insert_message(update.message)
-    update.message.reply_text("با تشکر پیام شما ثبت شد")
+def spam_callback(bot, update):
+    insert_spam(update.message)
+    update.message.reply_text("با تشکر پیام تبلیغاتی شما ثبت شد")
+    return ConversationHandler.END
+
+def nonspam_callback(bot, update):
+    insert_nonspam(update.message)
+    update.message.reply_text("با تشکر پیام غیرتبلیغاتی شما ثبت شد")
     return ConversationHandler.END
 
 def help(bot,  update):
@@ -49,11 +54,20 @@ def get_spam(bot, update):
 
 def get_nonspam(bot, update):
     update.message.reply_text("لطفا پیام غیرتبلیغاتی رو وارد کن.")
+    return msg
 
-def insert_message(msg):  
+def insert_spam(msg):  
     c = cursor() 
 
     query = f"INSERT INTO message VALUES ('{msg.message_id}','{msg.text}','spam', '1', null, '{msg.date}')"
+    c.cursor().execute(query)
+    c.commit()
+    c.close()
+
+def insert_nonspam(msg):  
+    c = cursor() 
+
+    query = f"INSERT INTO message VALUES ('{msg.message_id}','{msg.text}','nonspam', '1', null, '{msg.date}')"
     c.cursor().execute(query)
     c.commit()
     c.close()
@@ -63,13 +77,20 @@ def main():
 
     dp = updater.dispatcher
 
-    conv_handler = ConversationHandler(
+    spam_handler = ConversationHandler(
         entry_points=[CommandHandler('spam', get_spam), ],
-        states={msg: [MessageHandler(Filters.text, callback)]},
+        states={msg: [MessageHandler(Filters.text, spam_callback)]},
         fallbacks=[CommandHandler('cancel', cancel)]
     )
 
-    dp.add_handler(conv_handler)
+    nonspam_handler = ConversationHandler(
+        entry_points=[CommandHandler('nonspam', get_nonspam), ],
+        states={msg: [MessageHandler(Filters.text, nonspam_callback)]},
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(spam_handler)
+    dp.add_handler(nonspam_handler)
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
