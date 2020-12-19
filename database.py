26f2a7a83e4361178bead92801cd99c221ae6fcd
Payam Qorbanpour <payamqorbanpour@gmail.com>
2020-12-19 23:34:18 +0330

    Modify database file

--- database.py
+++ database.py
@@ -1,5 +1,22 @@
-import sqlite3
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
 
-def db_connection():
-    with sqlite3.connect('dejirbot.db') as conn:
-        return conn
+engine = create_engine('sqlite:///dejirbot.db', echo=True)
+Base = declarative_base()
+
+class MessageModel(Base):
+
+    __tablename__ = "message"
+    
+    id = Column(Integer, primary_key=True)
+    message = Column(String)
+    label = Column(String)
+    user_id = Column(String)
+    is_approved = Column(Boolean)
+    date = Column(Date)
+
+    def __init__(self):
+        self.date = datetime.utcnow()
+
+Base.metadata.create_all(engine)
--- migrations/message.py
+++
@@ -1,7 +0,0 @@
-def upgrade(connection):
-    cursor = connection.cursor()
-    cursor.execute('''CREATE TABLE message (id int, message text, label text, user_id int, is_approved bool, date text)''')
-    connection.commit()
-
-def downgrade(connection):
-    connection.execute('drop table animals')
