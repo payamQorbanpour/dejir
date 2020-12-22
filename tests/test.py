0e146393a06b94b873ed971c3d93095c362b588c
Payam Qorbanpour <payamqorbanpour@gmail.com>
2020-12-22 23:18:31 +0330

    Initiate unit tests

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestSpam(unittest.TestCase):
+
+    def test_get_spam(self):
+        want = msg
+        got = get_spam(bot, update)
+        self.assertEqual(got, wnat)
+
+
+if __name__ == "__main__":
+    unittest.main()
